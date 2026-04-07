7903907391578793fbc7c5ee35cd6f22f879cd3b
xJusTo <2513846@mail.ru>
2026-04-07 17:06:41 +0300

    Finalize Program.cs

--- lab1/task3/program.cs
+++ lab1/task3/program.cs
@@ -4,3 +4,5 @@ Console.Write("Введите первое число (A): ");
 double a = double.Parse(Console.ReadLine() ?? "0");
 Console.Write("Введите второе число (B): ");
 double b = double.Parse(Console.ReadLine() ?? "0");
+
+Console.WriteLine($"Результат сложения: {a} + {b} = {a + b}");
