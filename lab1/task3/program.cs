0ef87419e33b232735c90b89dcb7cfe46eca0781
KyisKyis <prostomatv@gmail.com>
2026-04-07 17:05:12 +0300

    Log 4 added to Program.cs

--- lab1/task3/program.cs
+++ lab1/task3/program.cs
@@ -3,3 +3,4 @@ using System;
 Console.Write("Введите первое число (A): ");
 double a = double.Parse(Console.ReadLine() ?? "0");
 Console.Write("Введите второе число (B): ");
+double b = double.Parse(Console.ReadLine() ?? "0");
